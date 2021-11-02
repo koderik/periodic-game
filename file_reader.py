83f9eb4c06abd49da43f5a5662188e7d67506d78
Erik <sandlov@ug.kth.se>
2021-11-02 10:42:52 +0100

    Added source file to read atom data from, added module to read from file

---
+++ file_reader.py
@@ -0,0 +1,11 @@
+# Module contains function for reading content of file and returning it as dictionary
+def read_file(file_name):
+    atom_dict = {}
+    with open(file_name, encoding="utf-8") as file:
+        next_name = ""
+        for line in file:
+            line_data = file.readline()
+            line_data = line_data.split()
+            name, weight = line_data[0], line_data[1]
+            atom_dict[name] = weight
+    return atom_dict
